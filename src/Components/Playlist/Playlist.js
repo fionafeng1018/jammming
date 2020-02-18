bebd29af5bda920f8266b94f3647bd8fa631b835
Fiona Feng <fiona.feng@pearson.com>
2020-02-18 15:10:31 -0500

    testing

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -8,7 +8,7 @@ class Playlist extends React.Component{
             <div className="Playlist">
                 <input defaultValue={'New Playlist'}/>
 
-                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+                <button className="Playlist-save">SAVE TO SPOTIFY </button>
             </div>
         )
     }
